452f7b54da6d928d160b67acd8d0bb487b9c4f58
Adrian Salceanu <adrian@MacBook-Pro.local>
2015-03-17 21:46:51 +0100

    add and modify (no tags operations) implemented

--- lib/pocketex/item.ex
+++ lib/pocketex/item.ex
@@ -3,8 +3,10 @@ defmodule Pocketex.Item do
   Wrapper functions for working with items (articles, images, videos) through the Pocket API
   """
 
-  @get_item_method_url  "https://getpocket.com/v3/get"
-  @request_headers_json [{"Content-Type", "application/json"}]
+  @get_item_method_url      "https://getpocket.com/v3/get"
+  @create_item_method_url   "https://getpocket.com/v3/add"
+  @modify_item_method_url   "https://getpocket.com/v3/send"
+  @request_headers_json     [{"Content-Type", "application/json"}]
 
   @doc """
   Gets items from the Pocket API, based on the optional parameters
@@ -53,19 +55,81 @@ defmodule Pocketex.Item do
   complete        = return all data about each item, including tags, images, authors, videos and more
   """
   def get(consumer_key, access_token, options \\ %{}) do
+    post(consumer_key, access_token, options, @get_item_method_url)
+  end
+
+  @doc """
+  Adds item to Pocket
+  """
+  def create(consumer_key, access_token, options \\ %{}) do
+    post(consumer_key, access_token, options, @create_item_method_url)
+  end
+
+  @doc """
+  Adds item to favorites
+  """
+  def fav(consumer_key, access_token, item_id) do
+    action(consumer_key, access_token, %{item_id: item_id, action: "favorite"})
+  end
+
+  @doc """
+  Remove item from favorites
+  """
+  def unfav(consumer_key, access_token, item_id) do
+    action(consumer_key, access_token, %{item_id: item_id, action: "unfavorite"})
+  end
+
+  @doc """
+  Archive item
+  """
+  def archive(consumer_key, access_token, item_id) do
+    action(consumer_key, access_token, %{item_id: item_id, action: "archive"})
+  end
+
+  @doc """
+  Unarchive item
+  """
+  def unarchive(consumer_key, access_token, item_id) do
+    action(consumer_key, access_token, %{item_id: item_id, action: "readd"})
+  end
+
+  @doc """
+  Delete item
+  """
+  def delete(consumer_key, access_token, item_id) do
+    action(consumer_key, access_token, %{item_id: item_id, action: "delete"})
+  end
+
+  defp action(consumer_key, access_token, options \\ %{}) do
+    {ms, s, _} = :os.timestamp
+    payload = [Dict.merge(options, %{time: (ms * 1_000_000 + s)})]
+    json = to_string(Poison.encode_to_iodata!(payload))
+    uri = @modify_item_method_url <> "?actions=" <> URI.encode_www_form(json) <>
+          "&access_token=#{access_token}&consumer_key=#{consumer_key}"
+
+    HTTPoison.start
+    HTTPoison.get!(uri)
+    |> Pocketex.Response.format_response
+  end
+
+  defp post(consumer_key, access_token, options, pocket_endpoint_url) do
     payload = Map.merge(%{"consumer_key" => consumer_key, "access_token" => access_token},
                         request_options(options))
     json = to_string(Poison.encode_to_iodata!(payload))
 
     HTTPoison.start
-    HTTPoison.post!(@get_item_method_url, json, @request_headers_json)
+    HTTPoison.post!(pocket_endpoint_url, json, @request_headers_json)
     |> Pocketex.Response.format_response
   end
 
   defp request_options(options) do
     unless Enum.empty?(options) do
       options = Enum.map(options, fn({k, v}) ->
-        {Pocketex.Utils.underscore_to_bumpy(Atom.to_string k), v}
+        key = cond do
+          is_atom(k)  -> Atom.to_string(k)
+          true        -> k
+        end
+        {Pocketex.Utils.underscore_to_bumpy(key), v}
       end)
       options = Enum.into(options, %{})
     end
