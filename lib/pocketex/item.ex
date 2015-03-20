0b802e6af057232e08c18f5aeed85bb44bcca60a
Adrian Salceanu <adrian@MacBook-Pro.local>
2015-03-20 21:24:10 +0100

    tag operations; convenience functions

--- lib/pocketex/item.ex
+++ lib/pocketex/item.ex
@@ -54,15 +54,29 @@ defmodule Pocketex.Item do
   simple          = only return the titles and urls of each item
   complete        = return all data about each item, including tags, images, authors, videos and more
   """
-  def get(consumer_key, access_token, options \\ %{}) do
+  def get(consumer_key, access_token, options) do
     post(consumer_key, access_token, options, @get_item_method_url)
+  end
+  def get(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    get(consumer_key, access_token, options)
+  end
+  def get(auth, options) do
+    get(Dict.merge(auth, options))
   end
 
   @doc """
   Adds item to Pocket
   """
-  def create(consumer_key, access_token, options \\ %{}) do
+  def create(consumer_key, access_token, options) do
     post(consumer_key, access_token, options, @create_item_method_url)
+  end
+  def create(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    create(consumer_key, access_token, options)
+  end
+  def create(auth, options) do
+    create(Dict.merge(auth, options))
   end
 
   @doc """
@@ -71,12 +85,26 @@ defmodule Pocketex.Item do
   def fav(consumer_key, access_token, item_id) do
     action(consumer_key, access_token, %{item_id: item_id, action: "favorite"})
   end
+  def fav(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    fav(consumer_key, access_token, options[:item_id])
+  end
+  def fav(auth, options) do
+    fav(Dict.merge(auth, options))
+  end
 
   @doc """
   Remove item from favorites
   """
   def unfav(consumer_key, access_token, item_id) do
     action(consumer_key, access_token, %{item_id: item_id, action: "unfavorite"})
+  end
+  def unfav(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    unfav(consumer_key, access_token, options[:item_id])
+  end
+  def unfav(auth, options) do
+    unfav(Dict.merge(auth, options))
   end
 
   @doc """
@@ -85,6 +113,13 @@ defmodule Pocketex.Item do
   def archive(consumer_key, access_token, item_id) do
     action(consumer_key, access_token, %{item_id: item_id, action: "archive"})
   end
+  def archive(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    archive(consumer_key, access_token, options[:item_id])
+  end
+  def archive(auth, options) do
+    archive(Dict.merge(auth, options))
+  end
 
   @doc """
   Unarchive item
@@ -92,12 +127,109 @@ defmodule Pocketex.Item do
   def unarchive(consumer_key, access_token, item_id) do
     action(consumer_key, access_token, %{item_id: item_id, action: "readd"})
   end
+  def unarchive(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    unarchive(consumer_key, access_token, options[:item_id])
+  end
+  def unarchive(auth, options) do
+    unarchive(Dict.merge(auth, options))
+  end
 
   @doc """
   Delete item
   """
   def delete(consumer_key, access_token, item_id) do
     action(consumer_key, access_token, %{item_id: item_id, action: "delete"})
+  end
+  def delete(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    delete(consumer_key, access_token, options[:item_id])
+  end
+  def delete(auth, options) do
+    delete(Dict.merge(auth, options))
+  end
+
+  @doc """
+  Adds tags to an item
+  """
+  def add_tags(consumer_key, access_token, item_id, tags) when is_list(tags) do
+    add_tags(consumer_key, access_token, item_id, Enum.join(tags, ", "))
+  end
+  def add_tags(consumer_key, access_token, item_id, tags) when is_bitstring(tags) do
+    tags_action(consumer_key, access_token, item_id, tags, "tags_add")
+  end
+  def add_tags(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    add_tags(consumer_key, access_token, options[:item_id], options[:tags])
+  end
+  def add_tags(auth, options) do
+    add_tags(Dict.merge(auth, options))
+  end
+
+  @doc """
+  Removes tags from an item
+  """
+  def remove_tags(consumer_key, access_token, item_id, tags) when is_list(tags) do
+    remove_tags(consumer_key, access_token, item_id, Enum.join(tags, ", "))
+  end
+  def remove_tags(consumer_key, access_token, item_id, tags) when is_bitstring(tags) do
+    tags_action(consumer_key, access_token, item_id, tags, "tags_remove")
+  end
+  def remove_tags(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    remove_tags(consumer_key, access_token, options[:item_id], options[:tags])
+  end
+  def remove_tags(auth, options) do
+    remove_tags(Dict.merge(auth, options))
+  end
+
+  @doc """
+  Replaces tags for an item
+  """
+  def replace_tags(consumer_key, access_token, item_id, tags) when is_list(tags) do
+    replace_tags(consumer_key, access_token, item_id, Enum.join(tags, ", "))
+  end
+  def replace_tags(consumer_key, access_token, item_id, tags) when is_bitstring(tags) do
+    tags_action(consumer_key, access_token, item_id, tags, "tags_replace")
+  end
+  def replace_tags(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    replace_tags(consumer_key, access_token, options[:item_id], options[:tags])
+  end
+  def replace_tags(auth, options) do
+    replace_tags(Dict.merge(auth, options))
+  end
+
+  @doc """
+  Removes all tags from item
+  """
+  def clear_tags(consumer_key, access_token, item_id) do
+    action(consumer_key, access_token, %{item_id: item_id, action: "tags_clear"})
+  end
+  def clear_tags(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    clear_tags(consumer_key, access_token, options[:item_id])
+  end
+  def clear_tags(auth, options) do
+    clear_tags(Dict.merge(auth, options))
+  end
+
+  @doc """
+  Ranames tag -- affects all items with this tag
+  """
+  def rename_tag(consumer_key, access_token, item_id, options) do
+    action(consumer_key, access_token, %{item_id: item_id, action: "tag_rename", old_tag: options[:old_tag], new_tag: options[:new_tag]})
+  end
+  def rename_tag(options) do
+    {consumer_key, access_token, options} = extract_options(options)
+    rename_tag(consumer_key, access_token, options[:item_id], options)
+  end
+  def rename_tag(auth, options) do
+    rename_tag(Dict.merge(auth, options))
+  end
+
+  defp tags_action(consumer_key, access_token, item_id, tags, action) do
+    action(consumer_key, access_token, %{item_id: item_id, action: action, tags: tags})
   end
 
   defp action(consumer_key, access_token, options \\ %{}) do
@@ -135,6 +267,13 @@ defmodule Pocketex.Item do
     end
 
     options
+  end
+
+  defp extract_options(options) do
+    %{consumer_key: consumer_key, access_token: access_token} = options
+    options = Dict.drop(options, [:consumer_key, :access_token])
+
+    {consumer_key, access_token, options}
   end
 
 end
--- test/pocketex_test.exs
+++ test/pocketex_test.exs
@@ -1,5 +1,6 @@
 defmodule PocketexTest do
   use ExUnit.Case, async: false
+  # doctest Pocketex.Item
 
   @consumer_key               Application.get_env(:pocket, :consumer_key)
   @redirect_uri               Application.get_env(:pocket, :redirect_uri)
@@ -12,8 +13,7 @@ defmodule PocketexTest do
   @access_token               "97d7d713-b89b-8a3c-79df-6c29e3" #DELETE_ME
 
   setup_all do
-    assert {:ok, _} = Pocketex.Item.create(@consumer_key, @access_token,
-                      %{url: @website_test_url, title: "Just Google", tags: "search engine, google, #{@test_delete_tag}"})
+    assert {:ok, _} = Pocketex.Item.create(@consumer_key, @access_token, %{url: @website_test_url, title: "Just Google", tags: "search engine, google, #{@test_delete_tag}"})
 
     on_exit fn ->
       assert {:ok, _} = Pocketex.Item.delete(@consumer_key, @access_token, get_pocket_item_id)
@@ -48,6 +48,10 @@ defmodule PocketexTest do
     assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{count: 1})
   end
 
+  test "get a random item from Pocket using a map of options" do
+    assert {:ok, _} = Pocketex.Item.get(%{consumer_key: @consumer_key, access_token: @access_token, count: 1})
+  end
+
   test "get Pocket item by tag, fav it and unfav it" do
     assert {:ok, _} = Pocketex.Item.fav(@consumer_key, @access_token, get_pocket_item_id)
     assert {:ok, _} = Pocketex.Item.unfav(@consumer_key, @access_token, get_pocket_item_id(1))
@@ -56,6 +60,88 @@ defmodule PocketexTest do
   test "get Pocket item by tag, archive it and unarchive it" do
     assert {:ok, _} = Pocketex.Item.archive(@consumer_key, @access_token, get_pocket_item_id)
     assert {:ok, _} = Pocketex.Item.unarchive(@consumer_key, @access_token, get_pocket_item_id(0, "archive"))
+  end
+
+  test "get Pocket item by tag and add one more tag as a string" do
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, get_pocket_item_id, "_hello_pocket_test_tag_")
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_hello_pocket_test_tag_"})
+  end
+
+  test "get Pocket item by tag and add two more tags as a string" do
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, get_pocket_item_id, "_hello_pocket_test_tag_, _bye_pocket_test_tag_")
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_bye_pocket_test_tag_"})
+  end
+
+  test "get Pocket item by tag and add one more tag as a list" do
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, get_pocket_item_id, ["_list_pocket_test_tag_"])
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_list_pocket_test_tag_"})
+  end
+
+  test "get Pocket item by tag and add two more tags as a list" do
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, get_pocket_item_id, ["_list_pocket_test_tag_", "_list_pocket_test_tag2_"])
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_list_pocket_test_tag2_"})
+  end
+
+  test "get Pocket item by tag, add some tags and remove some tags" do
+    item_id = get_pocket_item_id
+
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, item_id, ["_tag_1_", "_tag_2_", "_tag_3_"])
+
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_1_"})
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_2_"})
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_3_"})
+
+    # assert {:ok, _} = Pocketex.Item.remove_tags(@consumer_key, @access_token, get_pocket_item_id, ["_tag_1_", "_tag_2_"])
+    # removing multiple tags doesn't seem to work with the Pocket API
+    assert {:ok, _} = Pocketex.Item.remove_tags(@consumer_key, @access_token, item_id, ["_tag_1_"])
+    assert {:ok, _} = Pocketex.Item.remove_tags(@consumer_key, @access_token, item_id, "_tag_2_")
+
+    assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_1_"})
+    assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_2_"})
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_3_"})
+
+    assert {:ok, _} = Pocketex.Item.remove_tags(@consumer_key, @access_token, item_id, ["_tag_3_"])
+    assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_3_"})
+  end
+
+  test "get Pocket item by tag and replace some tags" do
+    item_id = get_pocket_item_id
+
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, item_id, ["_tag_1_"])
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_1_"})
+
+    assert {:ok, _} = Pocketex.Item.replace_tags(@consumer_key, @access_token, item_id, ["_tag_3_", @test_delete_tag])
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_3_"})
+    assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_1_"})
+
+    assert {:ok, _} = Pocketex.Item.remove_tags(@consumer_key, @access_token, item_id, ["_tag_3_"])
+  end
+
+  test "get Pocket item by tag and clear all tags" do
+    item_id = get_pocket_item_id
+
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, item_id, ["_tag_1_"])
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_1_"})
+
+    assert {:ok, _} = Pocketex.Item.clear_tags(@consumer_key, @access_token, item_id)
+
+    assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: "_tag_1_"})
+    assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: @test_delete_tag})
+
+    assert {:ok, _} = Pocketex.Item.add_tags(@consumer_key, @access_token, item_id, @test_delete_tag)
+  end
+
+  test "get Pocket item by tag and rename the tag" do
+    item_id = get_pocket_item_id
+
+    assert {:ok, _} = Pocketex.Item.rename_tag(@consumer_key, @access_token, item_id, %{old_tag: @test_delete_tag, new_tag: @test_delete_tag <> "_f"})
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: @test_delete_tag <> "_f"})
+    # assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: @test_delete_tag})
+    # why is this tag still here?!
+
+    assert {:ok, _} = Pocketex.Item.rename_tag(@consumer_key, @access_token, item_id, %{old_tag: @test_delete_tag <> "_f", new_tag: @test_delete_tag})
+    assert {:ok, %{"list" => []}} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: @test_delete_tag <> "_f"})
+    assert {:ok, _} = Pocketex.Item.get(@consumer_key, @access_token, %{tag: @test_delete_tag})
   end
 
   test "Pocket item should be unfav and unread" do
