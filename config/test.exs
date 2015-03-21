54df4decaaa3fb26683350076a99a26c351f34fc
Adrian Salceanu <adrian@MacBook-Pro.local>
2015-03-21 09:39:19 +0100

    consumer keys

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 
 config :pocket, redirect_uri: "http://localhost:4000/callbacks/pocket"
-config :pocket, consumer_key: "_your_consumer_key_here_" 
+config :pocket, consumer_key: "_your_consumer_key_here_"
